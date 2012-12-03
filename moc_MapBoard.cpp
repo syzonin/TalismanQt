9185cc19de0cc878afd3ba9fcd28419b9998bd63
Alex <Alex@192.168.2.11>
2012-12-02 19:12:59 -0500

    Implemented irregular sized MapSquares and character figure (resized card for now)...

--- moc_MapBoard.cpp
+++ moc_MapBoard.cpp
@@ -1,7 +1,7 @@
 /****************************************************************************
 ** Meta object code from reading C++ file 'MapBoard.h'
 **
-** Created: Sat Dec 1 23:33:48 2012
+** Created: Sun Dec 2 14:09:27 2012
 **      by: The Qt Meta Object Compiler version 62 (Qt 4.6.2)
 **
 ** WARNING! All changes made in this file will be lost!
--- moc_TheBigWindow.cpp
+++ moc_TheBigWindow.cpp
@@ -1,7 +1,7 @@
 /****************************************************************************
 ** Meta object code from reading C++ file 'TheBigWindow.h'
 **
-** Created: Sat Dec 1 23:33:49 2012
+** Created: Sun Dec 2 19:10:55 2012
 **      by: The Qt Meta Object Compiler version 62 (Qt 4.6.2)
 **
 ** WARNING! All changes made in this file will be lost!
